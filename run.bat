ca90139ee06ebc5ec45f1a885e8364bfe037681b
Fytex <pedropauloqt@gmail.com>
2023-05-10 23:39:27 +0100

    added new flag to batch

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 @echo off
-START /B /wait scripts/python-3.10.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0
-start inject.pyw
+START /B /wait scripts/python-3.10.0.exe /quiet InstallAllUsers=0 PrependPath=1 Include_test=0 InstallLauncherAllUsers=0
+START inject.pyw
 exit
